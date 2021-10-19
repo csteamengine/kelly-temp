792763ee20161640877c8cae40224399214e7370
Charlie Steenhagen <charlie.steenhagen@workiva.com>
2021-10-19 10:01:56 -0500

    Added image overview

--- resources/js/frontend/project.js
+++ resources/js/frontend/project.js
@@ -5,23 +5,12 @@ $('.project-image-tiles').on('click', function(event){
 });
 
 $(document).ready(function(){
-    $('.carousel-item').each(function(){
+    let carousel = $('.carousel-item');
+    carousel.each(function(){
         var imageElement = $(this);
         var image = $(this).data('image');
 
-        var img = $('<img />').attr({
-            'src': image,
-        }).on('load', function() {
-            var height = $(this).prop('naturalHeight');
-            var width = $(this).prop('naturalWidth');
-
-            imageElement.css('background', 'url("'+image+'") center center');
-            if(width > height){
-                imageElement.css('background-size', '100%');
-            }else{
-                imageElement.css('background-size', 'auto 100%');
-            }
-        });
+        loadImage(image, imageElement);
     });
 
     $('.project-image-tiles-image').each(function(){
@@ -34,5 +23,52 @@ $(document).ready(function(){
             imageElement.css('background-size', 'cover');
         });
     });
+
+    carousel.each(function(){
+        var imageElement = $(this);
+        var image = $(this).data('modal-image');
+
+        loadImage(image, imageElement);
+    });
 });
 
+function loadImage(image, imageElement){
+    $('<img />').attr({
+        'src': image,
+    }).on('load', function() {
+        var height = $(this).prop('naturalHeight');
+        var width = $(this).prop('naturalWidth');
+
+        imageElement.css('background', 'url("'+image+'") center center');
+        if(width > height){
+            imageElement.css('background-size', '100%');
+        }else{
+            imageElement.css('background-size', 'auto 100%');
+        }
+    });
+}
+
+$('#projectImagePreview').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget)
+    var image = button.data('modal-image');
+    var modal = $(this);
+    var element = modal.find('.modal-content');
+
+    let imgSrc = $('<img />').attr({
+        'src': image,
+        'class': 'img-responsive'
+    }).on('load', function() {
+        var height = $(this).prop('naturalHeight');
+        var width = $(this).prop('naturalWidth');
+
+        if(height > width){
+            $(this).css('height', '90vh');
+            $(this).css('width', 'auto');
+        }else{
+            $(this).css('height', 'auto');
+            $(this).css('width', '90vw');
+        }
+
+        element.html($(this));
+    });
+})
--- resources/views/frontend/projects/image_carousel.blade.php
+++ resources/views/frontend/projects/image_carousel.blade.php
@@ -12,7 +12,7 @@
             <div class="carousel-inner">
                 @foreach($images as $image)
                     @if($image->getTypeFromMime() != 'pdf')
-                        <div class="carousel-item @if($loop->first) active @endif h-100 m-auto" style="background-color: {{'#'.$image->getCustomProperty('color')}}; background-size: 100% !important" data-image="{{$image->getUrl('preview')}}">
+                        <div type="button" class="carousel-item @if($loop->first) active @endif h-100 m-auto" style="background-color: {{'#'.$image->getCustomProperty('color')}}; background-size: 100% !important" data-image="{{$image->getUrl('preview')}}" data-modal-image="{{$image->getUrl()}}" data-toggle="modal" data-target="#projectImagePreview">
                         </div>
                     @else
                         <div class="carousel-item @if($loop->first) active @endif h-100 m-auto" style="background-color: {{'#'.$image->getCustomProperty('color')}}; background-size: 100% !important" data-image="{{$image->getUrl('preview')}}" hidden>
@@ -28,6 +28,13 @@
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="projectImagePreview" aria-hidden="true" id="projectImagePreview">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
         </div>
     </div>
 </div>
