182a780f91185a04090d6559c8a3335ab40cb880
Charlie Steenhagen <charlie.steenhagen@workiva.com>
2021-10-19 10:33:06 -0500

    Found a way to ensure large images load last

--- resources/js/frontend/project.js
+++ resources/js/frontend/project.js
@@ -4,40 +4,44 @@ $('.project-image-tiles').on('click', function(event){
     $($(this).data('target')).get(0).scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
 
+let carouselLoaded = false;
+let tilesLoaded = false;
+
 $(document).ready(function(){
     let carousel = $('.carousel-item');
-    carousel.each(function(){
-        var imageElement = $(this);
-        var image = $(this).data('image');
+    let carouselLength = carousel.length;
+    let imageTiles = $('.project-image-tiles-image');
+    let imageTilesLength = imageTiles.length;
 
-        loadImage(image, imageElement);
+    carousel.each(function(index){
+        let imageElement = $(this);
+        let image = $(this).data('image');
+
+        loadImage(image, imageElement, (index === (carouselLength - 1)));
     });
 
-    $('.project-image-tiles-image').each(function(){
-        var imageElement = $(this);
-        var image = $(this).data('image');
-        var img = $('<img />').attr({
+    imageTiles.each(function(index){
+        let imageElement = $(this);
+        let image = $(this).data('image');
+        let img = $('<img />').attr({
             'src': image,
         }).on('load', function() {
             imageElement.css('background', 'url("'+image+'") center center');
             imageElement.css('background-size', 'cover');
+            if(index === (imageTilesLength - 1)){
+                tilesLoaded = true;
+                loadLargeImages();
+            }
         });
-    });
-
-    carousel.each(function(){
-        var imageElement = $(this);
-        var image = $(this).data('modal-image');
-
-        loadImage(image, imageElement);
     });
 });
 
-function loadImage(image, imageElement){
+function loadImage(image, imageElement, isLast = false){
     $('<img />').attr({
         'src': image,
     }).on('load', function() {
-        var height = $(this).prop('naturalHeight');
-        var width = $(this).prop('naturalWidth');
+        let height = $(this).prop('naturalHeight');
+        let width = $(this).prop('naturalWidth');
 
         imageElement.css('background', 'url("'+image+'") center center');
         if(width > height){
@@ -45,21 +49,37 @@ function loadImage(image, imageElement){
         }else{
             imageElement.css('background-size', 'auto 100%');
         }
+        if(isLast) {
+            carouselLoaded = true;
+            loadLargeImages();
+        }
     });
 }
 
+function loadLargeImages(){
+    if(tilesLoaded && carouselLoaded){
+        console.log("HERE!");
+        $('.carousel-item').each(function(){
+            let imageElement = $(this);
+            let image = $(this).data('modal-image');
+
+            loadImage(image, imageElement);
+        });
+    }
+}
+
 $('#projectImagePreview').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget)
-    var image = button.data('modal-image');
-    var modal = $(this);
-    var element = modal.find('.modal-content');
+    let button = $(event.relatedTarget)
+    let image = button.data('modal-image');
+    let modal = $(this);
+    let element = modal.find('.modal-content');
 
     let imgSrc = $('<img />').attr({
         'src': image,
         'class': 'img-responsive'
     }).on('load', function() {
-        var height = $(this).prop('naturalHeight');
-        var width = $(this).prop('naturalWidth');
+        let height = $(this).prop('naturalHeight');
+        let width = $(this).prop('naturalWidth');
 
         if(height > width){
             $(this).css('height', '90vh');
