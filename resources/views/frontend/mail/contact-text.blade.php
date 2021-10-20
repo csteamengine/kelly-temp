f5a9806a75fc82109e355a8f3be41b036f5943b2
Charlie Steenhagen <charlie.steenhagen@workiva.com>
2021-10-19 22:16:06 -0500

    Fixed mail name

--- resources/views/frontend/mail/contact-text.blade.php
+++ resources/views/frontend/mail/contact-text.blade.php
@@ -1,6 +1,6 @@
 @lang('validation.attributes.frontend.mail.body_title')
 
-@lang('validation.attributes.frontend.mail.name'): {{ $request->name }}
+@lang('validation.attributes.frontend.mail.name'): {{ $request->first_name . ' ' . $request->last_name}}
 @lang('validation.attributes.frontend.mail.email'): {{ $request->email }}
 @lang('validation.attributes.frontend.mail.phone'): {{ $request->phone ?? 'N/A' }}
 @lang('validation.attributes.frontend.mail.message'): {{ $request->message }}
--- resources/views/frontend/mail/contact.blade.php
+++ resources/views/frontend/mail/contact.blade.php
@@ -1,6 +1,6 @@
 <p>@lang('validation.attributes.frontend.mail.body_title')</p>
 
-<p><strong>@lang('validation.attributes.frontend.mail.name'):</strong> {{ $request->name }}</p>
+<p><strong>@lang('validation.attributes.frontend.mail.name'):</strong> {{ $request->first_name . ' ' . $request->last_name}}</p>
 <p><strong>@lang('validation.attributes.frontend.mail.email'):</strong> {{ $request->email }}</p>
 <p><strong>@lang('validation.attributes.frontend.mail.phone'):</strong> {{ $request->phone ?? 'N/A' }}</p>
 <p><strong>@lang('validation.attributes.frontend.mail.message'):</strong> {{ $request->message }}</p>
