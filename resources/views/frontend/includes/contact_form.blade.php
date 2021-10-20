d373dc5476c76863a02679d7e41e529ad33d8a95
Charlie Steenhagen <charlie.steenhagen@workiva.com>
2021-10-20 09:47:33 -0500

    Added @captcha back in

--- resources/views/frontend/includes/contact_form.blade.php
+++ resources/views/frontend/includes/contact_form.blade.php
@@ -35,23 +35,7 @@
 @if(config('boilerplate.access.captcha.contact'))
     <div class="row">
         <div class="col">
-            <script src="https://cdn.polyfill.io/v2/polyfill.min.js" type="application/javascript"></script><div id="_g-recaptcha"></div>
-            @if(config('boilerplate.access.captcha.configs.options.hidden'))
-                <style>.grecaptcha-badge{display:none;!important}</style>
-            @endif
-            <div class="g-recaptcha"
-                 data-sitekey="{{config('boilerplate.access.captcha.configs.site_key')}}"
-                 data-size="invisible"
-                 data-callback="_submitForm"
-                 data-badge="bottomright">
-            </div><script type="application/javascript" src="https://www.google.com/recaptcha/api.js" async defer></script><script type="application/javascript">var _submitForm,_captchaForm,_captchaSubmit,_execute=true;</script><script type="application/javascript">window.addEventListener("load", _loadCaptcha);function _loadCaptcha(){
-                    _captchaForm=document.querySelector("#_g-recaptcha").closest("form");
-                    _captchaSubmit=_captchaForm.querySelector('[type=submit]');
-                    _submitForm=function(){if(typeof _submitEvent==="function"){_submitEvent();grecaptcha.reset();}else{_captchaForm.submit();}};
-                    _captchaForm.addEventListener('submit',function(e){e.preventDefault();
-                        if(typeof _beforeSubmit==='function'){_execute=_beforeSubmit(e);}
-                        if(_execute){grecaptcha.execute();}});}</script>
-
+            @captcha
             <input type="hidden" name="captcha_status" value="true" />
         </div><!--col-->
     </div><!--row-->
